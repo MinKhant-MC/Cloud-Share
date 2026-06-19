445ff1e0951f3dac2cdf95357c4b2c29a267b0da
MinKhant-MC <sysadmin@mochaauthentic.com>
2026-06-19 18:19:17 +0630

    Create config.js

---
+++ config.js
@@ -0,0 +1,12 @@
+(function () {
+  'use strict';
+
+  window.MMC_CONFIG = Object.freeze({
+    API_URL: 'https://script.google.com/macros/s/AKfycbxOYeSC1HOiA_Q_6rMzbot1jFicP_aUo2EWfnHkpVWmRjX5Jt1p1yw25JN6MMMrt5VpOg/exec',
+    REQUEST_TIMEOUT_MS: 20000,
+    STORAGE_KEYS: Object.freeze({
+      USER_ID: 'mmc_user_id',
+      SESSION_TOKEN: 'mmc_session_token'
+    })
+  });
+})();
