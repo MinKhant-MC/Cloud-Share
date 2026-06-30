4da56eb0917ccd7b0cf2f728c802e76ab7acb127
MinKhant-MC <sysadmin@mochaauthentic.com>
2026-06-30 20:27:44 +0630

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
   'use strict';
 
   window.MMC_CONFIG = Object.freeze({
-    API_URL: 'https://script.google.com/macros/s/AKfycbyaIE0z26EO4Vm2uS7vbzF41MUgjPqHiJNVaA2XqXdZyXxJ_zDZ_cPxJIKzj7MaXnNgkg/exec',
+    API_URL: 'https://script.google.com/macros/s/AKfycbyG0nvON8UuQJ0tleGau1YtiH1rcc_0VFEzXFSxrE6lPVB7o9u7B_FUxDfcIXLfHrBBOA/exec',
     REQUEST_TIMEOUT_MS: 20000,
     STORAGE_KEYS: Object.freeze({
       USER_ID: 'mmc_user_id',
